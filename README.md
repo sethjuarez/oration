a2454e2933d39832604d3553d6a668a02efac0a7
Seth Juarez <seth.juarez@microsoft.com>
2021-11-29 15:05:48 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# baster
+Intended to be a starter project for a Next.js front-end with an Azure Functions TypeScript backend.
+
+# Running
+To run locally:
+
+1. `F5` to start api Function backend
+2. `swa start dev` from root to fire up front end and tie backend together
+
---
+++ api/liveness/index.ts
@@ -0,0 +1,15 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: {
+          "text": "server response"
+        }
+    };
+
+};
+
+export default httpTrigger;
